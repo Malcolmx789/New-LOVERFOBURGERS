da06fa24ca6cec00c9172fd6d35e0f34ed070b55
Malcolm Simmons <Malcolm.Simmons789@yahoo.com>
2022-04-26 15:42:26 -0600

    Add files via upload

---
+++ LOVEBURGERS/recipe/main.js
@@ -0,0 +1,18 @@
+// show more btn
+function showMore1() {
+  let x = document.getElementById('toggleBtn1');
+  if (x.style.display === 'block') {
+    x.style.display = 'none';
+  } else {
+    x.style.display = 'block';
+  }
+}
+function showMore2() {
+  let x = document.getElementById('toggleBtn2');
+  if (x.style.display === 'block') {
+    x.style.display = 'none';
+  } else {
+    x.style.display = 'block';
+  }
+}
+// End show more
